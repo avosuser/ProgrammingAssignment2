6baeb6718b664a04e720bb5b138dd82e8e53c48c
avosuser <valmiki.anand@gmail.com>
2014-09-17 21:56:53 -0500

    My submission for assignment 2
    Here is my version of the code for CacheMatrix and cachesolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # set i "inverse" to NULL
+    i <- NULL
+  
+    # the y argument in the set function initializes 
+    # x from the parent env.
+    set <- function(y = matrix()) {
+    x <<- y
+    i <<- NULL
+    }
+  
+    # the get function just returns x
+    get <- function() x
+  
+    # the setinv function just sets the value of i either from the parent environment or global environment
+   setinv <- function(inverse) i <<- inverse
+   
+   getinv <- function() i
+  
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
 }
 
 
@@ -12,4 +31,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inv <- x$getinv()
+		if(!is.null(inv)) {
+			message("Getting cached data")
+			return(inv)
+		}
+  
+		data <- x$get()
+  
+		if(length(data[,1]) != length(data[1,])) {
+			stop("Inverse of a matrix can be done only on a square matrix")
+		}
+   
+		inv <- solve(data)
+		x$setinv(inv)
+		inv		
 }
